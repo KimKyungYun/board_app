9db374b081eefc961d25c202696a02290e93736a
KimDoToRi <60084235+KimKyungYun@users.noreply.github.com>
2023-07-03 18:00:27 +0900

    feat: scss 수정중

--- src/components/common/TopNavigation/index.tsx
+++ src/components/common/TopNavigation/index.tsx
@@ -1,9 +1,11 @@
-import {Link} from 'react-router-dom';
+import {Link,useLocation} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../../assets/Icon/board_icon.svg';
 import styles from './TopNavigation.module.scss';
 
 export default function TopNavigation(){
   const auth=true;
+  const location=useLocation();
+  
   return(
     <nav className={styles.navbar}>
       <div className={styles.navbar__logo}>
@@ -14,7 +16,8 @@ export default function TopNavigation(){
         <ul className={styles.navbar__links}>
           <li><Link to='/list' className={styles.navbar__link}>게시판</Link></li>
           <li><Link to='/post' className={styles.navbar__link}>글쓰기</Link></li>
-          {auth ? <li><Link to='/login' className={styles.navbar__link}>로그인</Link></li>:''}
+          {auth ? (location.pathname ==='/login' ? <li><Link to='/signup' className={styles.navbar__link}>회원가입</Link></li>: 
+          <li><Link to='/login' className={styles.navbar__link}>로그인</Link></li>) :''}
         </ul>
     </nav>
   )
--- src/pages/BoardList/index.tsx
+++ src/pages/BoardList/index.tsx
@@ -1,10 +1,17 @@
-export default function BoardList(){
-  return(
-    <div>
-      목록
-      <ol>
-        <li>1</li>
-      </ol>
+import styles from "./BoardList.module.scss";
+export default function BoardList() {
+  return (
+    <div className={styles.content}>
+      <div className={styles.list}>
+        목록
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
