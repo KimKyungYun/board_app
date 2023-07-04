dee7090ce9306fb0d79b351841c763a2c9393a8d
KimDoToRi <60084235+KimKyungYun@users.noreply.github.com>
2023-07-04 18:00:31 +0900

    fix: 오류수정

--- src/pages/Auth/Signup/components/IdInput.tsx
+++ src/pages/Auth/Signup/components/IdInput.tsx
@@ -1,18 +1,8 @@
 import cn from "utils/ts/className";
-import { ReactComponent as ErrorIcon } from "assets/svg/auth/error.svg";
-import { useFormContext } from "react-hook-form";
-import { checkIdDuplicate } from "api/user";
-import { ERROR_MESSAGE } from "pages/Auth/Signup/static/signUp";
-import styles from "../SignUp.module.scss";
-import { SignUpFormData } from "../static/signUp";
+import { ReactComponent as ErrorIcon } from "assets/Icon/error.svg";
+import styles from "../Signup.module.scss";
 
 export default function IdInput() {
-  const {
-    register,
-    watch,
-    formState: { errors },
-  } = useFormContext<SignUpFormData>();
-
   return (
     <div className={styles.form__form}>
       <label className={styles.form__label} htmlFor="id-input">
@@ -21,37 +11,17 @@ export default function IdInput() {
           <ErrorIcon
             className={cn({
               [styles["form__error-icon"]]: true,
-              [styles["form__error-icon--active"]]: errors?.id !== undefined,
             })}
             aria-hidden
           />
-          {errors.id?.message}
         </div>
       </label>
       <input
         placeholder="아이디를 입력하세요"
         id="id-input"
-        className={cn({
-          [styles.form__input]: true,
-          [styles["form__input--id"]]: true,
-          [styles["form__input--error"]]: errors?.id !== undefined,
-        })}
         // TODO: 아이디 중복확인 기능
-        {...register("id", {
-          required: ERROR_MESSAGE.id,
-          validate: {
-            checkValid: (val) => val === "" || "아이디 중복확인을 해주세요.",
-          },
-        })}
       />
-      <button
-        type="button"
-        className={cn({
-          [styles["form__id-check-button"]]: true,
-          [styles["form__id-check-button--active"]]: watch("id") !== "",
-          [styles["form__id-check-button--error"]]: errors?.id !== undefined,
-        })}
-      >
+      <button type="button" className={cn({})}>
         중복 확인
       </button>
     </div>
--- src/pages/Auth/Signup/index.tsx
+++ src/pages/Auth/Signup/index.tsx
@@ -1,13 +1,12 @@
 import styles from "./Signup.module.scss";
+import IdInput from "./components/IdInput";
 
 export default function Signup() {
   return (
     <div className={styles.signup}>
       <form action="submit" className={styles.form}>
         <span className={styles.form__title}>회원가입</span>
-        <label>
-          <input type="text" className={styles.form__input} />
-        </label>
+        <IdInput />
         <label>
           <input type="password" className={styles.form__input} />
         </label>
